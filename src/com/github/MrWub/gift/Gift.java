92bb625e4fba09d7bf59f0140e236c50bac19360
wyt <914554688@qq.com>
2016-06-07 12:34:36 +0800

    2016.6.7 12:34 完工 未调试版本

--- src/com/github/MrWub/gift/Gift.java
+++ src/com/github/MrWub/gift/Gift.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -70,9 +71,9 @@ public class Gift extends JavaPlugin {
 	
 	public boolean onCommand(CommandSender sender,Command cmd,String label,String[] args) {
 		if (label.equalsIgnoreCase("gift")) {
-			if (args.length == 1) return false;
-			if (args.length == 2) {
-				if (args[1].equalsIgnoreCase("help")) {
+			if (args.length == 0) return false;
+			if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("help")) {
 					String[] strs ={"gift命令帮助",
 									"/gift list 查看礼包列表 需要权限gift.admin",
 									"/gift get X 获取礼包X 需要权限gift.admin或gift.get.X",
@@ -81,16 +82,16 @@ public class Gift extends JavaPlugin {
 									"/gift give X Y 给X玩家一个Y礼包 需要权限gift.admin"};
 					sender.sendMessage(strs);
 				} else 
-					if (args[1].equalsIgnoreCase("list")) {
-						if (sender.hasPermission("gift.admin")) {
-							for (String key:gifts.keySet()) {
-								sender.sendMessage(key);
-							}
-						} else sender.sendMessage("无权操作");
-					}
+				if (args[0].equalsIgnoreCase("list")) {
+					if (sender.hasPermission("gift.admin")) {
+						for (String key:gifts.keySet()) {
+							sender.sendMessage(key);
+						}
+					} else sender.sendMessage("无权操作");
 				}
-			if (args.length == 3) {
-				if (args[1].equals("all-add")) {
+			}
+			if (args.length == 2) {
+				if (args[0].equals("all-add")) {
 					if (sender.hasPermission("gift.admin")) {
 						if (sender instanceof Player) {
 							Player p = (Player)sender;
@@ -98,15 +99,56 @@ public class Gift extends JavaPlugin {
 							for (ItemStack item:p.getInventory()) {
 								tmp.add(addItem(item));
 							}
-							addGifts(args[2], tmp);
-							p.sendMessage("成功创建礼包 " + args[2]);
+							addGifts(args[1], tmp);
+							p.sendMessage("成功创建礼包 " + args[1]);
 							
 						} else info("控制台不支持");
+					} else sender.sendMessage("无权操作");
+				} else 
+				if (args[0].equalsIgnoreCase("get")) {
+					if (sender.hasPermission("gift.admin") || sender.hasPermission("gift.get."+args[1])){
+						if (sender instanceof Player) {
+							 Player p = (Player)sender;
+							 if (gifts.containsKey(args[1])) {
+								 giveGift(p, args[1]);
+							 } else sender.sendMessage("礼包不存在");
+						}else sender.sendMessage("控制台不支持");
+					}else sender.sendMessage("无权操作");
+				}
+				if (args[1].equalsIgnoreCase("del")) {
+					if (sender.hasPermission("gift.admin")) {
+						if (gifts.containsKey(args[1])) {
+							delGifts(args[1]);
+							sender.sendMessage("已删除礼包" + args[1]);
+						} else sender.sendMessage("礼包不存在");
+					} else sender.sendMessage("无权操作");
+				}
+			}
+			if (args.length == 3) {
+				if (args[0].equalsIgnoreCase("give")) {
+					if (sender.hasPermission("gift.admin")) {
+						Player p = Bukkit.getPlayer(args[1]);
+						if (p != null) {
+							if (gifts.containsKey(args[2])) {
+								giveGift(p, args[2]);
+								sender.sendMessage("已将礼包" + args[2] + "发送至" + args[1]);
+							} else sender.sendMessage("礼包不存在");
+						} else sender.sendMessage("玩家不存在或不在线");
 					} else sender.sendMessage("无权操作");
 				}
 			}
 		}
-		return false;
+		return true;
+	}
+	private void giveGift(Player p, String name) {
+		 p.sendMessage("获得礼包 " + name);
+		 for (int id:gifts.get(name)) {
+			 if (!(p.getInventory().addItem(items.get(id))).isEmpty()) {
+				p.sendMessage("背包空间不足，物品掉落在地");
+				p.getWorld().dropItem(p.getLocation(), items.get(id));
+			 };
+		 }
+		
 	}
 	private void delGifts(String name) {
 		gifts.remove(name);
